e8b8c282a038871b0bafafb4cf913a74e459d542
WlfromB <sasha567883@gmail.com>
2024-07-13 14:43:33 +0300

    liquibase migrations added

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,16 +1,13 @@
 package com.example.demo;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableCaching
-public class Demo1Application {
-    public static void main(String[] args) {
+public class Demo1Application {    
+    public static void main(String[] args) {       
         SpringApplication.run(Demo1Application.class, args);
     }
 }
