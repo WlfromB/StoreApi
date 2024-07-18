d65514919c8c2d6a3540471dc2a004ad873b0b75
WlfromB <sasha567883@gmail.com>
2024-07-18 15:42:41 +0300

    #1 exclude filtering token auth/login and some refactor logger

--- src/main/java/com/example/demo/security/JwtFilter.java
+++ src/main/java/com/example/demo/security/JwtFilter.java
@@ -16,17 +16,27 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtFilter extends GenericFilterBean {
     private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final List<String> EXCLUDE_URLS = List.of("/auth/login");
+    
     private final JwtProvider jwtProvider;
-
+    
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        if (isExcludedUrl(request)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+        
         final String token = getTokenFromRequest((HttpServletRequest) servletRequest);
         if(token != null && jwtProvider.validateAccessToken(token)){
             final Claims claims = jwtProvider.getAccessClaims(token);
@@ -35,6 +45,11 @@ public class JwtFilter extends GenericFilterBean {
             SecurityContextHolder.getContext().setAuthentication(jwtAuthentication);
         }
         filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private boolean isExcludedUrl(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return EXCLUDE_URLS.stream().anyMatch(path::startsWith);
     }
     
     private String getTokenFromRequest(HttpServletRequest request) {
